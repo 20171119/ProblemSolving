b7ac293eb5b1d2f12e83ab1aea21d751c8e12825
jspark98 <jspark98@unist.ac.kr>
2022-04-07 18:14:46 +0900

    BOJ 14889

--- BOJ/14889.py
+++ BOJ/14889.py
@@ -1,5 +1,5 @@
 # 실버2 스타트와 링크
-from itertools import combinations
+from itertools import combinations, permutations
 
 N = int(input())
 S = []
@@ -12,4 +12,10 @@ result = 100000
 for i in range(int(len(players) / 2)):
     start = players[i]
     link = players[len(players) - i - 1]
-    a, b = 0
+    a, b = 0, 0
+    for x, y in list(permutations(list(start), 2)):
+        a += S[x - 1][y - 1]
+    for x, y in list(permutations(list(link), 2)):
+        b += S[x - 1][y - 1]
+    result = min(result, abs(b - a))
+print(result)
