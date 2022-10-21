291daaf16f81f04923ccbb3455b24678609e7501
jongseo98 <jspark98@unist.ac.kr>
2022-10-21 22:55:36 +0900

    백준 1문제

---
+++ BOJ/15988.py
@@ -0,0 +1,14 @@
+# 실버2 1,2,3 더하기 3
+import sys
+input = sys.stdin.readline
+t = int(input())
+dp = [0] * 1000000
+dp[1] = 1
+dp[2] = 2
+dp[3] = 4
+for i in range(4, 1000000):
+    dp[i] = (dp[i-1] + dp[i-2] + dp[i-3]) % 1000000009
+
+for _ in range(t):
+    n = int(input())
+    print(dp[n])
