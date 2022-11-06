c25f3f6fe7bac990a10c5958fee68364635ab621
jongseo98 <jspark98@unist.ac.kr>
2022-11-06 16:21:43 +0900

    백준 1문제

---
+++ BOJ/1916.py
@@ -0,0 +1,33 @@
+# 골드5 최소비용 구하기
+import sys
+import heapq
+n = int(input())
+m = int(input())
+graph = [[] for _ in range(n+1)]
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    graph[a].append((b,c))
+
+start, end = map(int, input().split())
+
+INF = int(1e9)
+distance = [INF] * (n+1)
+
+def dijkstra():
+    q = []
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
+
+    while q:
+        dist, now = heapq.heappop(q)
+        if distance[now] < dist:
+            continue
+
+        for i in graph[now]:
+            cost = i[1] + dist
+            if distance[i[0]] > cost:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
+
+dijkstra()
+print(distance[end])
