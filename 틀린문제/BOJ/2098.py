b8c9871cc670db7031de697d5d5fece86c18fd08
jongseo98 <jspark98@unist.ac.kr>
2022-11-07 21:40:28 +0900

    백준 1문제

--- 틀린문제/BOJ/2098.py
+++ 틀린문제/BOJ/2098.py
@@ -1,31 +1,32 @@
 # 골드1 외판원 순회
-n = int(input())
+def tsp(i, route):
+    if dp[i][route] != 0:
+        return dp[i][route]
 
-graph = []
-for i in range(n):
-    graph.append(list(map(int, input().split())))
-
-INF = int(1e9)
-dp = [[INF] * (1 << n) for _ in range(n)] # 비트마스크 형식으로 거리 초기화 - ex) 4라면 0000 ~ 1111 까지 16개
-
-def dfs(x, visited): # (현재 방문 도시, 방문했던 도시 집합)
-    if visited == (1 << n) - 1:     # 모든 도시를 방문했다면
-        if graph[x][0]:             # 출발점으로 가는 경로가 있을 때
-            return graph[x][0]
-        else:                       # 출발점으로 가는 경로가 없을 때
+    if route == (1 << (n-1)) - 1:
+        if graph[i][0]:
+            return graph[i][0]
+        else:
             return INF
 
-    if dp[x][visited] != INF:       # 이미 최소비용이 계산되어 있다면
-        return dp[x][visited]
+    min_route = INF
 
-    for i in range(1, n):           # 모든 도시를 탐방
-        if not graph[x][i]:         # 가는 경로가 없다면 skip
+    for j in range(1, n):
+        if not graph[i][j]:
             continue
-        if visited & (1 << i):      # 이미 방문한 도시라면 skip
+        if route & (1 << j-1):
             continue
+        D = graph[i][j] + tsp(j, route | (1 << (j-1)))
+        if min_route > D:
+            min_route = D
+    
+    dp[i][route] = min_route
 
-        # 점화식 부분(위 설명 참고)
-        dp[x][visited] = min(dp[x][visited], dfs(i, visited | (1 << i)) + graph[x][i])
-    return dp[x][visited]
+    return min_route
 
-print(dfs(0, 1))
+n = int(input())
+graph = [list(map(int, input().split())) for _ in range(n)]
+dp = [[0] * (1 << n-1) for _ in range(n)]
+INF = int(1e9)
+
+print(tsp(0,0))
